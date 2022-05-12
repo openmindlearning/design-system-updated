ef08ede88fe97687c9b4f50f375f83ffd4a9310b
Jose Alvarez <j.alvarez11@icloud.com>
2022-05-12 15:58:25 -0400

    Format files

--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -21,11 +21,7 @@ export const Popover = ({
 Props<any>): ReactElement => {
   const popperRef = useRef<HTMLDivElement>(null);
 
-  const { styles, attributes } = usePopper(
-    referenceRef.current,
-    popperRef.current,
-    popperOptions
-  );
+  const { styles, attributes } = usePopper(referenceRef.current, popperRef.current, popperOptions);
 
   return (
     <div ref={popperRef} style={styles.popper} {...attributes.popper}>
