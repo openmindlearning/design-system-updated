5c27a52f73c2fd2ce4d47fad79bd7b80e4400d5f
Madeline Griswold <madelinegr@users.noreply.github.com>
2022-05-12 12:04:42 -0700

    updates to project card

--- src/components/TimelineProjectCard/TimelineProjectCard.tsx
+++ src/components/TimelineProjectCard/TimelineProjectCard.tsx
@@ -2,74 +2,131 @@ import * as styles from "./TimelineProjectCard.css";
 
 import React, { ReactElement, ReactNode } from "react";
 import { pluralize } from "../../util";
-import { ClapIcon, LoveIcon, WowIcon } from "../../icons";
+import {
+	ClapIcon,
+	LightBulbIcon,
+	LoveIcon,
+	OneHundredIcon,
+	WowIcon,
+} from "../../icons";
 
 export const TEST_LABELS = {
-  TIMELINE_PROJECT_CARD: "timeline_project_card",
+	TIMELINE_PROJECT_CARD: "timeline_project_card",
 };
 
+export interface ReactionCounts {
+	comment?: number;
+	like?: number;
+	wow?: number;
+	clap?: number;
+	one_hundred?: number;
+	genius?: number;
+}
+
 interface Props {
-  project: {
-    type: string;
-    label: string;
-    title: string;
-    description: string;
-  };
-  attention: {
-    numReactions: number;
-    numComments: number;
-  };
-  mediaSlot?: ReactNode;
-  onClick: () => void;
+	project: {
+		type: string;
+		label: string;
+		title: string;
+		description: string;
+	};
+	reactionCounts: ReactionCounts;
+	mediaSlot?: ReactNode;
+	onClick: () => void;
 }
 
 export const TimelineProjectCard = ({
-  project: { type, label, title, description },
-  attention: { numReactions, numComments },
-  mediaSlot,
-  onClick = () => ({}),
+	project: { type, label, title, description },
+	reactionCounts,
+	mediaSlot,
+	onClick = () => ({}),
 }: Props): ReactElement => {
-  const reactionsText = `${numReactions} ${pluralize(numReactions, "reaction", "reactions")}`;
-  const commentsText = `${numComments} ${pluralize(numComments, "comment", "comments")}`;
+	const numReactions =
+		(reactionCounts?.clap ?? 0) +
+		(reactionCounts?.genius ?? 0) +
+		(reactionCounts?.like ?? 0) +
+		(reactionCounts?.one_hundred ?? 0) +
+		(reactionCounts?.wow ?? 0);
+	const numComments = 0; // TODO: backend still needs to return comment counts
+	const reactionsText = `${numReactions} ${pluralize(
+		numReactions,
+		"reaction",
+		"reactions"
+	)}`;
+	const commentsText = `${numComments} ${pluralize(
+		numComments,
+		"comment",
+		"comments"
+	)}`;
 
-  return (
-    <div
-      data-testid={TEST_LABELS.TIMELINE_PROJECT_CARD}
-      className={styles.outerContainer}
-      {...{ onClick }}
-    >
-      <div className={styles.innerContainer}>
-        <div className={styles.contentContainer}>
-          <div className={styles.titleLabelContainer}>
-            <div className={styles.type}>{type}</div>
-            <div className={styles.labelContainer}>
-              <div className={styles.label} title={label}>
-                {label}
-              </div>
-            </div>
-          </div>
-          <div className={styles.titleDescriptionContainer}>
-            <div className={styles.title}>{title}</div>
-            <div className={styles.description}>{description}</div>
-          </div>
+	return (
+		<div
+			data-testid={TEST_LABELS.TIMELINE_PROJECT_CARD}
+			className={styles.outerContainer}
+			{...{ onClick }}
+		>
+			<div className={styles.innerContainer}>
+				<div className={styles.contentContainer}>
+					<div className={styles.titleLabelContainer}>
+						<div className={styles.type}>{type}</div>
+						<div className={styles.labelContainer}>
+							<div className={styles.label} title={label}>
+								{label}
+							</div>
+						</div>
+					</div>
+					<div className={styles.titleDescriptionContainer}>
+						<div className={styles.title}>{title}</div>
+						<div className={styles.description}>{description}</div>
+					</div>
 
-          <div className={styles.attentionContainer}>
-            <WowIcon width={16} height={16} className={styles.reactionIcon} />
-            <LoveIcon width={16} height={16} className={styles.reactionIcon} />
-            <ClapIcon width={16} height={16} className={styles.reactionIcon} />
-            <div
-              className={styles.reactionsCommentsContainer}
-              title={`${reactionsText} · ${commentsText}`}
-            >
-              {reactionsText}
-              <span className={styles.dotSeparator}>·</span>
-              {commentsText}
-            </div>
-          </div>
-        </div>
+					<div className={styles.attentionContainer}>
+						{reactionCounts?.wow && reactionCounts.wow > 0 && (
+							<WowIcon width={16} height={16} className={styles.reactionIcon} />
+						)}
+						{reactionCounts?.like && reactionCounts.like > 0 && (
+							<LoveIcon
+								width={16}
+								height={16}
+								className={styles.reactionIcon}
+							/>
+						)}
+						{reactionCounts?.clap && reactionCounts.clap > 0 && (
+							<ClapIcon
+								width={16}
+								height={16}
+								className={styles.reactionIcon}
+							/>
+						)}
+						{reactionCounts?.genius && reactionCounts.genius > 0 && (
+							<LightBulbIcon
+								width={16}
+								height={16}
+								className={styles.reactionIcon}
+							/>
+						)}
+						{reactionCounts?.one_hundred && reactionCounts.one_hundred > 0 && (
+							<OneHundredIcon
+								width={16}
+								height={16}
+								className={styles.reactionIcon}
+							/>
+						)}
+						<div
+							className={styles.reactionsCommentsContainer}
+							title={`${reactionsText} · ${commentsText}`}
+						>
+							{numReactions > 0 && reactionsText}
+							{numComments > 0 && numReactions > 0 && (
+								<span className={styles.dotSeparator}>·</span>
+							)}
+							{numComments > 0 && commentsText}
+						</div>
+					</div>
+				</div>
 
-        {mediaSlot && <div className={styles.mediaSlot}>{mediaSlot}</div>}
-      </div>
-    </div>
-  );
+				{mediaSlot && <div className={styles.mediaSlot}>{mediaSlot}</div>}
+			</div>
+		</div>
+	);
 };
