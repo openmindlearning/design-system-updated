034676993cc2356a2cd7e0b3e81dc7981b3966d1
Justin Poliachik <jpoliachik@gmail.com>
2022-05-17 10:01:32 -0400

    add viewbox to diamond icon (#19)

--- src/icons/base/DiamondIcon.tsx
+++ src/icons/base/DiamondIcon.tsx
@@ -16,6 +16,7 @@ export const DiamondIcon = ({
   return (
     <svg
       {...{ width, height }}
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
