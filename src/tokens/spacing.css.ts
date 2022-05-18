44cff5b046a2242c4f17318e5307c14da2d0e20a
Colby Thomas <coloradocolby@gmail.com>
2022-05-18 11:01:43 -0600

    px -> rem

--- src/tokens/spacing.css.ts
+++ src/tokens/spacing.css.ts
@@ -1,12 +1,20 @@
+/**
+ *  In CSS rem stands for “root em”, a unit of measurement that represents the
+ *  font size of the root element. The default root element font size on
+ *  browsers is 16px.
+ *
+ *  NOTE: The comments below only apply to that default 16px root element
+ *  font-size.
+ */
 export const spacing = {
-  "1x": "4px",
-  "2x": "8px",
-  "3x": "12px",
-  "4x": "16px",
-  "5x": "24px",
-  "6x": "32px",
-  "7x": "40px",
-  "8x": "48px",
-  "9x": "56px",
-  "10x": "64px",
+  "1x": "0.25rem", // 4px
+  "2x": "0.5rem", // 8px
+  "3x": "0.75rem", // 12px
+  "4x": "1rem", // 16px
+  "5x": "1.5rem", // 24px
+  "6x": "2rem", // 32px
+  "7x": "2.5rem", // 40px
+  "8x": "3rem", // 48px
+  "9x": "3.5rem", // 56px
+  "10x": "4rem", // 64px
 };
