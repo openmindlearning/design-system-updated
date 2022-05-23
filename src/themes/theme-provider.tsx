813d3a0f9c1e06d1a91602593aecbf61bf5169b0
Jose Alvarez <j.alvarez11@icloud.com>
2022-05-23 12:23:55 -0400

    fix: explicitly type ThemeWrapper children

--- src/themes/theme-provider.tsx
+++ src/themes/theme-provider.tsx
@@ -14,9 +14,10 @@ export const ThemeContext = createContext<ThemeContextType>({
 });
 
 // This component can be used when the theme is controlled by the parent of the component (vs global context)
-export const ThemeWrapper: React.FC<{ theme: ThemeType }> = ({ theme, children }) => (
-  <div className={theme === ThemeType.light ? themeClass : darkThemeClass}>{children}</div>
-);
+export const ThemeWrapper: React.FC<{ theme: ThemeType; children: React.ReactNode }> = ({
+  theme,
+  children,
+}) => <div className={theme === ThemeType.light ? themeClass : darkThemeClass}>{children}</div>;
 
 export interface ThemeProviderProps {
   children: React.ReactNode;
